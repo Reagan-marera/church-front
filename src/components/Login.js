dc93af5440433e6532329ea4474f50b335b6f065
reagan <marierareagan@gmail.com>
2024-12-25 11:56:45 +0300

    errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = ({ setToken, setRole, setTransactions }) => {
     };
 
     try {
-      const response = await fetch('htps://finance.boogiecoin.com /login', {
+      const response = await fetch('http://localhost:5000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
